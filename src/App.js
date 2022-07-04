614d10c772a24be895b0ba265c5173ae75c59e76
Rapoporras <nacho.porras@lgfm.app>
2022-07-04 19:34:00 +0200

    first commint

--- src/App.js
+++ src/App.js
@@ -1,23 +1,157 @@
-import logo from './logo.svg';
-import './App.css';
-
+import logo from "./logo.svg";
+import "./App.css";
+import Header from "./Header";
+import {
+  Box,
+  Button,
+  Center,
+  Heading,
+  HStack,
+  Image,
+  List,
+  ListIcon,
+  ListItem,
+  SimpleGrid,
+  StackDivider,
+  Text,
+  VStack,
+} from "@chakra-ui/react";
+import { AiFillCar } from "react-icons/ai";
+import Features from "./Feature";
+import Pricing from "./Pricing";
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+      <Header></Header>
+      <Center
+        w="100%"
+        bg="#E9F0F6"
+        minHeight="100vh"
+        color="#002327"
+        p="15px"
+        // position="relative"
+      >
+        <SimpleGrid columns={[1, null, 2]} spacing="40px">
+          <Center>
+            <Box maxW={"500px"}>
+              <Heading as="h2" size="2xl">
+                (2xl) In love with React & Next
+              </Heading>
+              <Text fontSize="xl" mt="15px">
+                (xl) In love with React & Next
+              </Text>
+              <Button colorScheme="blue" mt="15px">
+                Button
+              </Button>
+            </Box>
+          </Center>
+
+          <Image
+            w="100%"
+            objectFit="cover"
+            shadow={"sm"}
+            borderRadius="1em"
+            src="https://firebasestorage.googleapis.com/v0/b/autoescuelaclickanddrive.appspot.com/o/driving-instructor-showing-vehicle-dashboard-and-buttons-to-the-student-taking-driving-lessons.jpg?alt=media&token=d50d1a7e-9ce7-473b-a629-84bb8e73cfc8"
+            alt="Dan Abramov"
+          />
+        </SimpleGrid>
+        {/* <div className="wave"></div> */}
+      </Center>
+      <Center
+        w="100%"
+        bg="#fff"
+        p="15px"
+        minHeight="100vh"
+        height="auto"
+        color="#002327"
+      >
+        <SimpleGrid columns={[1, null, 2]} spacing="40px">
+          <Center>
+            <Box maxW={"500px"}>
+              <Heading as="h2" size="2xl">
+                (2xl) In love with React & Next
+              </Heading>
+              <Text fontSize="xl" mt="15px">
+                (xl) In love with React & Next
+              </Text>
+              <List spacing={15} mt="15px">
+                <ListItem>
+                  <ListIcon as={AiFillCar} color="green.500" />
+                  Lorem ipsum dolor sit amet, consectetur adipisicing elit
+                </ListItem>
+                <ListItem>
+                  <ListIcon as={AiFillCar} color="green.500" />
+                  Assumenda, quia temporibus eveniet a libero incidunt suscipit
+                </ListItem>
+                <ListItem>
+                  <ListIcon as={AiFillCar} color="green.500" />
+                  Quidem, ipsam illum quis sed voluptatum quae eum fugit earum
+                </ListItem>
+                {/* You can also use custom icons from react-icons */}
+                <ListItem>
+                  <ListIcon as={AiFillCar} color="green.500" />
+                  Quidem, ipsam illum quis sed voluptatum quae eum fugit earum
+                </ListItem>
+              </List>
+            </Box>
+          </Center>
+
+          <Image
+            objectFit="cover"
+            src="https://firebasestorage.googleapis.com/v0/b/autoescuelaclickanddrive.appspot.com/o/Group%201.png?alt=media&token=35084f70-5926-41cd-94bf-1307b7ec78f8"
+            alt="Dan Abramov"
+          />
+        </SimpleGrid>
+      </Center>
+      <Center w="100%" bg="#00BAB5" p="5px 5px 5px 0xpx" height="auto" color="#002327">
+        <SimpleGrid columns={[1, null, 2]} spacing="40px">
+          <Box>
+            <Text>Hola</Text>
+          </Box>
+          <Box
+            w="100%"
+            as="video"
+            controls
+            borderRadiusBottomLeft="3em"
+            src="https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4"
+            poster="https://peach.blender.org/wp-content/uploads/title_anouncement.jpg?x11217"
+            alt="Big Buck Bunny"
+            objectFit="contain"
+            sx={{
+              aspectRatio: "16/9",
+            }}
+          />
+        </SimpleGrid>
+      </Center>
+      <Center
+        w="100%"
+        bg="#fff"
+        p="15px"
+        minHeight="100vh"
+        color="#002327"
+        mt="15px"
+        textAlign="center"
+      >
+        <SimpleGrid
+          columns={[1, null, 1]}
+          spacing="40px"
+          style={{ alignSelf: "center" }}
         >
-          Learn React
-        </a>
-      </header>
+          <Heading as="h2" size="2xl">
+            (2xl) In love with React & Next
+          </Heading>
+          <Text fontSize="xl" mt="15px">
+            (xl) In love with React & Next
+          </Text>
+          <Features></Features>
+        </SimpleGrid>
+      </Center>
+      <Center w="100%" bg="#E9F0F6" p="15px" minHeight="100vh" color="#002327">
+        <Pricing></Pricing>
+      </Center>
+      <Center w="100%" bg="#00BAB5" p="15px" height="300px" color="#002327">
+        This is the Center
+      </Center>
     </div>
   );
 }
---
+++ src/firebase.js
@@ -0,0 +1,21 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyAPx5Wtm0RGIGcH2_MG8Lh5ugPNKPm6ie8",
+  authDomain: "autoescuelaclickanddrive.firebaseapp.com",
+  projectId: "autoescuelaclickanddrive",
+  storageBucket: "autoescuelaclickanddrive.appspot.com",
+  messagingSenderId: "159450973030",
+  appId: "1:159450973030:web:c9f3fe05e4b64dcf437ea7",
+  measurementId: "G-GHMRF6WXZ5"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
